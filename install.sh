cc1a45a1f4af58a60455fa6d96e91e82badad638
Richard Ma <richard.ma.19850509@gmail.com>
2016-02-21 23:11:45 +0800

    script add cron job

--- install.sh
+++ install.sh
@@ -7,3 +7,10 @@ mkdir -p ~/backup
 chmod a+x ./mysql-auto-backup.sh ./auto-remove-outofdate-backup.sh
 
 # TODO add mysql-auto-backup.sh to cron job
+#write out current crontab
+#crontab -l > mycron
+#echo new cron into cron file
+#echo "00 09 * * 1-5 echo hello" >> mycron
+#install new cron file
+#crontab mycron
+#rm mycron
